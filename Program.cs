8b5e7a01ff8cb546a5cfe96909c741c60dd1aeb9
MudBoxCode <mehdimosavi.shalhe@gmail.com>
2023-10-16 18:51:50 +0200

    First commit just to see if it works

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Team");
+//This is a commit to the master branch please make your own branch and code away in said branch 
