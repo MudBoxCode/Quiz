56256cc2b395f93af40c8ad0d6ee4ca24685283a
MudBoxCode <mehdimosavi.shalhe@gmail.com>
2023-10-23 08:01:41 +0200

    It works now i think

--- Program.cs
+++ Program.cs
@@ -1,27 +1,38 @@
 ﻿using System;
 
-namespace Quiz
+namespace Program
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int score = 0;
+            
             List<Question> questions = new List<Question>();
-
-            questions.Add(new Year("In what year was the first Star Wars movie released?", "5", 1977));
-            questions.Add(new Year("In what year was the first Star Trek movie released?", "5", 1979));
-            questions.Add(new Year("In what year was the first Alien movie released?", "5", 1979));
+            int score = 0;
+            string userAnswer;
+            questions.Add(new Year("In what year was the first Star Wars movie released?", 5, 1977));
+            questions.Add(new Year("In what year was the first Star Trek movie released?", 5, 1979));
+            questions.Add(new Year("In what year was the first Alien movie released?", 5, 1979));
 
             foreach (Question question in questions)
             {
-                Console.WriteLine(question.QuestionText);
-                string answer = Console.ReadLine();
-                if (question.IsCorrect(answer))
+                Console.WriteLine(question);
+                userAnswer = Console.ReadLine();    
+
+                if (question.CheckAnswer(userAnswer))
                 {
-                    score += int.Parse(question.Points);
+                    Console.WriteLine("Correct!");
+                    score += question.Points;
                 }
+                else
+                {
+                    Console.WriteLine("Incorrect!");
+                }
+
             }
+
+            Console.WriteLine($"Your score is {score}."); 
+
         }
     }
 }
--- Question.cs
+++ Question.cs
@@ -1,23 +1,39 @@
 
 abstract class Question
 {
-    protected string QuestionText { get; set; }
-    protected string Points { get; set; }
+    public string QuestionText { get; set; }
+    public int Points { get; set; }
 
-    public Question(string questionText, string points)
+    public Question(string questionText, int points)
     {
         QuestionText = questionText;
         Points = points;
+    }
+
+    public override string ToString()
+    {
+        return $"Points: {Points}. Question: {QuestionText}";
+    }
+
+    public virtual bool CheckAnswer(string answer)
+    {
+        return true;
     }
 }
 
 class Year : Question
 {
-    protected int CorrectAnswer { get; set; }
+    public int CorrectAnswer { get; set; }
 
 
-    public Year(string questionText, string points, int correctAnswer) : base(questionText, points)
+    public Year(string questionText, int points, int correctAnswer) : base(questionText, points)
     {
         CorrectAnswer = correctAnswer;
+    }
+
+    public override bool CheckAnswer(string answer)
+    {
+        int answerInt = int.Parse(answer);
+        return answerInt == CorrectAnswer;
     }
 }
